76493b1f4ab9dabaedb7bb5ac7a41c5416d07b76
hayttle <hayttle@gmail.com>
2023-07-28 21:59:04 -0300

    fix: server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/*": "/$1"
@@ -13,3 +15,6 @@ server.use(router)
 server.listen(3000, () => {
   console.log("JSON Server is running")
 })
+
+// Export the Server API
+module.exports = server
